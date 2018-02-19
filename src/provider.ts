d015b628dbc6311dd0ec9368726d3e3ed46175cc
Gennadiy Khatuntsev <e.steelcat@gmail.com>
2018-02-19 11:51:36 +0500

    Loading manifest

--- src/provider.ts
+++ src/provider.ts
@@ -1,6 +1,8 @@
+import * as EventEmitter from 'eventemitter3';
 import Hls from 'hls.js';
 
-import { isSupportSrc } from './util';
+import { JWPlayerEvents } from './events';
+import { formatLevelLabel, isSupportSrc } from './util';
 
 export interface ISource {
     type: string;
@@ -41,8 +43,10 @@ export default class Provider {
     protected player: any;
     protected attached = false;
     protected element: HTMLElement;
+    protected container: HTMLElement;
     protected video: HTMLVideoElement;
     protected hls: Hls;
+    protected eventEmitter: EventEmitter;
 
     constructor(id) {
         this.player = window.jwplayer(id);
@@ -56,16 +60,138 @@ export default class Provider {
 
         this.video.className = 'jw-video jw-reset';
 
-        this.hls = new Hls({});
-
         if (this.player) {
+            let config = this.player.getConfig();
+            let hlsConfig = config.hlsjsConfig || {};
+
+            this.hls = new Hls(hlsConfig);
             this.player.provider = this;
             this.player.hls = this.hls;
+
+            this.hls.on(Hls.Events.MANIFEST_LOADED, this.onManifestLoaded.bind(this));
         }
+
+        this.eventEmitter = new EventEmitter();
     }
 
     public init() {
-        // nope
+        this.video.setAttribute('jw-loaded', 'init');
+    }
+
+    public load(item) {
+        if (Provider.disabled) {
+            return;
+        }
+
+        let source = (item.sources.find(s => s.default) || item.sources[0]).file;
+
+        this.video.load();
+        this.hls.loadSource(source);
+
+        if (!this.attached) {
+            this.attachMedia();
+        }
+    }
+
+    public play() {
+        this.video.play();
+    }
+
+    public attachMedia() {
+        this.attached = true;
+        this.hls.attachMedia(this.video);
+    }
+
+    public detachMedia() {
+        this.attached = false;
+    }
+
+    public on(event: string, listener: EventEmitter.ListenerFn) {
+        this.eventEmitter.on(event, listener);
+    }
+
+    public off(event: string, listener: EventEmitter.ListenerFn) {
+        this.eventEmitter.off(event, listener);
+    }
+
+    public trigger(event, ...args) {
+        if (!this.attached) {
+            return;
+        }
+
+        this.eventEmitter.emit.apply(this.eventEmitter, [event, ...args]);
+
+        if (event !== 'all') {
+            args.unshift('all');
+
+            this.eventEmitter.emit.apply(this.eventEmitter, [event, ...args]);
+        }
+    }
+
+    public setContainer(element: HTMLElement) {
+        this.container = element;
+        this.container.appendChild(this.video);
+    }
+
+    protected onManifestLoaded() {
+        let levels = this.hls.levels;
+
+        this.trigger(JWPlayerEvents.JWPLAYER_MEDIA_LEVELS, {
+            currentQuality: this.getCurrentLevel().jw,
+            levels: this.getLevels()
+        });
+
+        if (!levels) {
+            return;
+        }
+
+        this.trigger(JWPlayerEvents.JWPLAYER_MEDIA_TYPE, {
+            mediaType: this.isVideo(levels) ? 'video' : 'audio'
+        });
+    }
+
+    protected isVideo(levels): boolean {
+        let isVideo: number = 0;
+
+        levels.forEach(level => {
+            isVideo += +!!(level.videoCodec || !level.audioCodec &&
+                (level.bitrate > 64000 || level.width || level.height));
+        });
+
+        return !!isVideo;
+    }
+
+    protected getCurrentLevel(level?: number) {
+        let levels = this.hls.levels || [];
+
+        level = level || this.hls.currentLevel;
+
+        return {
+            jw: this.hls.manual_level === -1 || levels.length < 2 ? 0 : level + 1,
+            real: levels.length < 2 ? 0 : level + 1
+        };
+    }
+
+    protected getLevels() {
+        let levels = this.hls.levels || [];
+        let result = [];
+
+        if (levels.length > 1) {
+            result.push({
+                label: 'Auto'
+            });
+        }
+
+        levels.forEach(level => {
+            result.push({
+                bitrate: level.bitrate,
+                height: level.height,
+                label: formatLevelLabel(level),
+                width: level.width
+            });
+        });
+
+        return result;
     }
 
 }
--- src/util.ts
+++ src/util.ts
@@ -7,3 +7,19 @@ export function isSupportSrc(src: ISource): boolean {
 
     return !!(src.file || '').match(/\.m3u8$/);
 }
+
+export function formatLevelLabel(level) {
+    if (level.height) {
+        return level.height + 'p';
+    }
+
+    if (level.width) {
+        return Math.round(level.width * 9 / 16) + 'p';
+    }
+
+    // if (level.bitrate) {
+    //     return scaled_number(level.bitrate) + 'bps';
+    // }
+
+    return 0;
+}
